8a21aea7f5b36d5b2ecfe2b7c44ed932e414f511
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-04 15:38:33 -0800

    debug db connection

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,20 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
 
 self.addEventListener("install", function (evt) {
     // pre cache transaction data
     evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction/bulk"))
       );
       
     // pre cache all static assets
